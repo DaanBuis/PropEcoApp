8381ce048bf4dd11fbbfdbe36cdfb6f33f47de16
Daan <dbuis201@caledonian.ac.uk>
2025-03-17 16:15:46 +0000

    fixin

--- src/components/Home.js
+++ src/components/Home.js
@@ -89,7 +89,7 @@ function Home() {
     <div style={{ alignItems:"center", justifyContent:"center", display:"flex"}}>
      {!loading && searchResults && (
       <div style={{width: "700px", height: "500px"}}>
-      <MapContainer center={[searchResults.location.latitude, searchResults.location.longitude]} zoom={zoomLevel} style={{ height: "500px", width: "100%",margin: "0 auto", border:"3px solid #4CAF50", borderRadius: "10px", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)" }}>
+      <MapContainer center={[searchResults.location.latitude, searchResults.location.longitude]} zoom={zoomLevel} style={{ height: "500px", width: "100%",margin: "0 auto", border:"3px solid #4CAF50", borderRadius: "10px", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)", paddingTop:"30px" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -99,7 +99,7 @@ function Home() {
             <Marker position={[searchResults.location.latitude, searchResults.location.longitude]} icon={icon}>
               <Popup>
                 <div>
-                  <h4>{searchResults.location.admin_boundaries_country_name}</h4>
+                  <h4>{searchResults.location.admin_boundaries.country_name}</h4>
                   <p><strong>In Conservation Area ? </strong> {searchResults.planning.conservation_areas.in_conservation_area ? 'Yes' : 'No'}</p>
                   <p><strong>Rainfall:</strong> {searchResults.climate.historical.average_rainfall.value.toFixed(2)}</p>
                 
